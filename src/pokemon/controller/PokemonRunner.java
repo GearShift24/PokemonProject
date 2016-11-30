971614316830853db496729405d7333885286518
GearShift24 <jwri6847@740s-pl-369.local>
2016-11-30 07:40:56 -0700

    added panel, frame, and Pokemon model

---
+++ src/pokemon/controller/PokemonRunner.java
@@ -0,0 +1,10 @@
+package pokemon.controller;
+
+public class PokemonRunner
+{
+	public static void main (String [] args)
+	{
+		PokemonController appController = new PokemonController();
+		appController.start();
+	}
+}
---
+++ src/pokemon/view/PokemonPanel.java
@@ -0,0 +1,36 @@
+package pokemon.view;
+
+import javax.swing.*;
+import pokemon.controller.PokemonController;
+
+public class PokemonPanel extends JPanel
+{
+
+	public PokemonPanel(PokemonController baseController)
+	{
+		
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	public void setupLayout()
+	{
+		
+	}
+	
+	
+	public void setupListeners()
+	{
+		
+	}
+}
